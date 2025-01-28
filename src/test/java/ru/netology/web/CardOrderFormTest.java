a23224b7cfe5a24d9db688c8be3aa98ed8b764f9
KseniaYudina <uksenia359@gmail.com>
2025-01-28 14:23:47 +0300

    modified CardOrderFormTest

--- src/test/java/ru/netology/web/CardOrderFormTest.java
+++ src/test/java/ru/netology/web/CardOrderFormTest.java
@@ -20,7 +20,7 @@ public class CardOrderFormTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
 
